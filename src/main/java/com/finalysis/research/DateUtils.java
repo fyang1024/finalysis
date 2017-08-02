1b05631959d56e2672e2d11667acfb918e06296c
yangf <workinghard>
2017-08-02 22:47:21 +1000

    refactor

--- src/main/java/com/finalysis/research/DateUtils.java
+++ src/main/java/com/finalysis/research/DateUtils.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
